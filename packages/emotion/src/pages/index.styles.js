f21b1285064a613e659601c3a70b521ba7c8f657
prftst2022 <prftst2022@protonmail.com>
2022-04-07 20:11:03 +0300

    remove warning

--- packages/emotion/src/pages/index.styles.js
+++ packages/emotion/src/pages/index.styles.js
@@ -1,7 +1,6 @@
 import { css } from "@emotion/react"
 
-export const wrapper = theme =>
-  css`
-    margin: 0 auto;
-    max-width: 1200px;
-  `
+export const wrapper = css`
+  margin: 0 auto;
+  max-width: 1200px;
+`
