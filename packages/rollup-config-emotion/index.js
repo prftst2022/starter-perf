4a84ac248fea99f9cf980648132095e0e3a81e94
prftst2022 <prftst2022@protonmail.com>
2022-04-13 23:24:44 +0300

    fix rollup

--- packages/rollup-config-emotion/index.js
+++ packages/rollup-config-emotion/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-import babel from "rollup-plugin-babel"
+import { babel } from "@rollup/plugin-babel"
 import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
-// import external from "rollup-plugin-peer-deps-external"
-// import json from "@rollup/plugin-json"
 import path from "path"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
 // import { terser } from 'rollup-plugin-terser';
 
 const PACKAGE_ROOT_PATH = process.cwd()
@@ -13,20 +13,24 @@ const PACKAGE_ROOT_PATH = process.cwd()
 const INPUT_FILE = path.join(PACKAGE_ROOT_PATH, "src/index.js")
 const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, "package.json"))
 
-// const EXTERNAL_DEPS = [
-//   ...Object.keys(PKG_JSON.peerDependencies || {}),
-//   ...Object.keys(PKG_JSON.dependencies || {}),
-// ]
+const EXTERNAL_DEPS = [
+  ...Object.keys(PKG_JSON.peerDependencies || {}),
+  ...Object.keys(PKG_JSON.dependencies || {}),
+]
 
 const plugins = [
+  peerDepsExternal(),
+  json(),
+  nodeResolve({
+    preferBuiltins: true,
+  }),
   babel({
     exclude: "node_modules/**",
-    presets: ["@babel/preset-env", "@babel/preset-react"],
+    babelHelpers: "bundled",
+    presets: ["@babel/preset-env", "@babel/preset-react", "@emotion/babel-preset-css-prop"],
     plugins: ["@emotion/babel-plugin"],
   }),
   commonjs(),
-  nodeResolve(),
-  // terser(),
 ]
 
 const config = {
@@ -36,7 +40,7 @@ const config = {
     { file: PKG_JSON.module, format: "esm" },
   ],
   plugins,
-  external: ['react', '@emotion/core', '@emotion/styled'],
+  external: EXTERNAL_DEPS,
 }
 
 export default [config]
